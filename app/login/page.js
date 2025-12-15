8922b93d15a5c09d6fbbb989ff37ca4d848af697
Ashawstrand <Ashley.Shaw-Strand@edu.sait.ca>
2025-12-15 13:47:54 -0700

    Basic login styling implemented

--- app/login/page.js
+++ app/login/page.js
@@ -1,7 +1,49 @@
 export default function Login() {
+  
+  
+  
+  
+  
+  
+  
+  
+  
   return (
     <div className="min-h-screen flex items-center justify-center bg-black text-white">
-      <h1 className="text-3xl font-bold">Login Page Coming Soon</h1>
+      <div className="bg-black p-8 rounded-lg shadow-lg w-full max-w-md">
+        <form className="flex flex-col space-y-4">
+          <label className="font-medium" htmlFor="email">
+            Email
+          </label>
+          <input
+            className="w-full px-3 py-2 rounded border border-zinc-700 bg-black text-white placeholder:opacity-70 focus:outline-none focus:ring-2 focus:ring-red-600 rounded "
+            placeholder="Enter email"
+          ></input>
+
+          <label>Password</label>
+          <input
+            className="w-full px-3 py-2 rounded border border-zinc-700 bg-black text-white placeholder:opacity-70 focus:outline-none focus:ring-2 focus:ring-red-600 rounded "
+            placeholder="Enter password"
+          ></input>
+            <button
+          className="w-full py-2 rounded bg-red-600 hover:bg-red-700 transition-colors font-semibold mt-5"
+          type="submit"
+        >
+          Login
+        </button>
+        </form>
+
+      
+
+         <div className="mt-6 text-center">
+          <p className="text-sm text-zinc-400">Don't have an account?</p>
+          <button
+className="mt-2 w-full py-2 rounded bg-black border border-red hover:bg-zinc-900 transition-colors font-semibold"
+          >
+          Sign Up 
+          </button>
+      </div>
+      </div>
     </div>
   );
 }
--- app/movieDetails/page.js
+++ app/movieDetails/page.js
@@ -63,7 +63,15 @@ export default function MovieDetails() {
   return (
     <div className="bg-black min-h-screen text-white px-6 py-10">
       <header className="w-full px-6 py-4 flex items-center justify-between bg-black">
-        <Image src="/FrameLogo.png" alt="Frame logo" width={300} height={300} />
+        <Link href="/" className="flex items-center">
+          <Image
+            className="w-48 sm:w-64 md:w-72 lg:w-80 h-auto"
+            src="/FrameLogo.png"
+            alt="Frame logo"
+            width={300}
+            height={300}
+          />
+        </Link>
         <Link
           href="/login"
           className="flex items-center justify-center mr-4 px-8 py-4 text-xl font-semibold rounded-full bg-red-600 text-white hover:bg-red-700 transition-colors"
