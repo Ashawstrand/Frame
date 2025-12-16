157d1816b53793ef9496edc0bd04e2dcb36f0124
Ashawstrand <Ashley.Shaw-Strand@edu.sait.ca>
2025-12-16 14:02:08 -0700

    created favorites functionalities

--- app/favorites/page.js
+++ app/favorites/page.js
@@ -1,7 +1,38 @@
-export default function Favorites() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-black text-white">
-      <h1 className="text-3xl font-bold">Favorites Page Coming Soon</h1>
+"use client";
+import { useState, useEffect } from "react";
+import Image from "next/image";
+
+
+export default function FavoritesPage(){
+
+  const [favorites, setFavorites] = useState([]);
+
+
+  useEffect(() => {
+    async function loadFavorites() {
+      const favs = await getFavorites();
+      setFavorites(favs);
+    }
+    loadFavorites();
+  }, []);
+
+  return(
+    <div className="bg-black min-h-screen text-white p-6">
+      <h1 className="text-2xl font-bold mb-6">My Favorites</h1>
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-6">
+        {favorites.map(movie => (
+          <div key={movie.movieId} className="flex flex-col items-center">
+            <Image
+              src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+              alt={movie.title}
+              width={150}
+              height={225}
+              className="rounded-lg shadow-lg"
+            />
+            <p className="mt-2 text-center">{movie.title}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
---
+++ app/utils/favoritesFunctions.js
@@ -0,0 +1,50 @@
+import { doc, getDocs , setDoc, deleteDoc, collection } from "firebase/firestore";
+import { auth, db } from "./firebase";
+
+
+export async function addToFavorites(movie) {
+    const user = auth.currentUser;
+    if (!user) return;
+    const favesRef = doc(db, "users", user.uid, "favorites", movie.id.toString());
+    await setDoc(favesRef, {
+        movieId: movie.id,
+        title: movie.title,
+        poster_path: movie.poster_path,
+        release_date: movie.release_date,
+    });
+}
+
+
+export async function removeFromFavorites(movieId) {
+    const user = auth.currentUser;
+    if (!user) return;
+    const favesRef = doc(db, "users", user.uid, "favorites", movieId.toString());
+    await deleteDoc(favesRef);
+}
+
+
+export async function getFavorites() {
+    const user = auth.currentUser;
+    if (!user) return [];
+    const favesRef = collection(db, "users", user.uid, "favorites");
+    const snapshot = await getDocs(favesRef);
+    return snapshot.docs.map(doc => doc.data());
+}
+
+
+export async function toggleFavorite(movie) {
+    const user = auth.currentUser;
+    if (!user) return false;
+
+    const favRef = doc(db, "users", user.id, "favorites", movie.id.toString());
+    const existing = await getDoc(favRef);
+
+    if (existing.exists()) {
+        await removeFromFavorites(movie.id);
+        return false;
+    }
+    else{
+        await addToFavorites(movie);
+        return true;
+    }
+}
